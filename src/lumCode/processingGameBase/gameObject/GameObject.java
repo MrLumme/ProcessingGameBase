8649ad2dc185a5ead3dcf6d025c38a45aa9ccfcd
Lumme Thomsen <mclumme@gmail.com>
2025-09-04 08:45:39 +0200

    Added GameObject

---
+++ src/lumCode/processingGameBase/gameObject/GameObject.java
@@ -0,0 +1,31 @@
+public Class GameObject {
+	private static int idCounter = 0;
+	
+	private PApplet p;
+	
+	public final int id;
+	public int depth;
+	public float x, y;
+	public Sprite sprite;
+
+  public GameObject() {
+		id = idCounter;
+		idCounter++;
+		p = Main.instance()
+  }
+
+	public GameObject(float x, float y) {
+		this();
+		this.x = x;
+		this.y = y;
+	}
+
+	public GameObject(float x, float y, Sprite sprite) {
+		this(x, y);
+		this.sprite = sprite;
+	}
+
+	public void draw() {
+		sprite.drawAt(x, y);
+	}
+}
